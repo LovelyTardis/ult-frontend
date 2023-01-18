5f7477b9c3c258463e047ed63c040db388ed645f
Albert <albert.tardis05@gmail.com>
2023-01-18 13:43:15 +0100

    Create try connection helper

---
+++ src/helpers/tryConnection.js
@@ -0,0 +1,14 @@
+const tryConnection = async () => {
+  try {
+    await fetch(`${import.meta.env.DEV_API}`);
+    return true;
+  } catch (_) {
+    return {
+      code: 500,
+      error: true,
+      data: "No connection with the server api.",
+    };
+  }
+};
+
+export default tryConnection;
