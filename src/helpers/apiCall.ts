252e90db5a16ae8a00666bfdc92e3282164b0e80
Albert <albert.tardis05@gmail.com>
2023-01-24 17:55:43 +0100

    Add more reliable functionality

--- src/helpers/apiCall.ts
+++ src/helpers/apiCall.ts
@@ -1,11 +1,35 @@
-export const apiCall = async (path: string, method: string = "GET") => {
-  const call = await fetch(`${import.meta.env.DEV_API}${path}`, {
-    method,
-  });
-  const jsonCall = await call.json();
-  const { ...res } = jsonCall;
+export const apiCall = async (
+  path: string,
+  method: string = "GET",
+  body: object = {}
+) => {
+  let call = null;
+  const url = "http://localhost:8000";
 
-  return {
-    ...res,
-  };
+  try {
+    switch (method) {
+      case "POST":
+        call = await fetch(`${url}${path}`, {
+          method,
+          body: JSON.stringify(body),
+          headers: {
+            "Content-Type": "application/json",
+            "user-token": localStorage.getItem("user-token") || "",
+          },
+        });
+        break;
+      default:
+        call = await fetch(`${url}${path}`);
+        break;
+    }
+
+    const jsonCall = await call.json();
+    const { ...res } = jsonCall;
+
+    return {
+      ...res,
+    };
+  } catch (error) {
+    console.error(error);
+  }
 };
