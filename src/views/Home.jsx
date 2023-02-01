47e86b86192141bf9b09f9929eaff0d87d172119
Albert <albert.tardis05@gmail.com>
2023-02-01 19:26:43 +0100

    Implement ult create component

--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -1,24 +1,10 @@
-// import Layout from "../layouts/Layout.jsx";
-// import UltContainer from "../components/ult/UltContainer.astro";
-// import ErrorDisplay from "../components/ErrorDisplay.astro";
-// import { apiCall, tryConnection } from "../helpers/index";
-
-// const connected = await tryConnection();
-// const { error, code, data } =
-//   connected !== true ? connected : await apiCall("/ult");
+import UltCreate from "../components/ult/UltCreate";
 
 export default function Home() {
   return (
     <>
       <h1>HOME PAGE</h1>
-      <h2>Aqui hay cosas</h2>
+      <UltCreate />
     </>
-    // <Layout title="Ult - index" mainHeader="Home">
-    //   {!error ? (
-    //     <UltContainer data={data} />
-    //   ) : (
-    //     <ErrorDisplay message={data} code={code} />
-    //   )}
-    // </Layout>
   );
 }
