4824fc7f42891886dc7fb2fbc4ecc025f6a3be0c
Albert <albert.tardis05@gmail.com>
2023-02-01 19:19:59 +0100

    Add feature: ult creation component

--- src/components/ult/UltCreate.jsx
+++ src/components/ult/UltCreate.jsx
@@ -1,66 +1,59 @@
-// ---
-// const apiUrl = import.meta.env.DEV_API;
-// ---
+import React from "react";
+import { useState } from "react";
+import { apiCall } from "../../helpers/apiCall";
+import styles from "./UltCreate.module.css";
 
-{
-  /* <form class="create-ult">
-  <textarea
-    name="ult-text"
-    placeholder="Write your feelings..."
-    autocapitalize="sentences"
-    minlength={1}
-    maxlength={100}></textarea>
-  <button id="create-ult" type="submit"> Post</button>
-</form>
+export default function UltCreate() {
+  const [formValues, setFormValues] = useState({
+    message: "",
+  });
 
-<style>
-  .create-ult {
-    display: flex;
-    flex-direction: column;
-    row-gap: 10px;
-    justify-content: space-between;
-  }
-  textarea {
-    width: auto;
-    height: 100px;
-    resize: none;
-    font-family: "Segoe UI", Tahoma, Geneva, Verdana, sans-serif;
-    font-size: medium;
-  }
-  button {
-    width: 100px;
-  }
-</style>
-
-<script define:vars={{ apiUrl }}>
-  const form = document.querySelector("form");
-
-  form.addEventListener("submit", (e) => {
+  const handleSubmit = (e) => {
     e.preventDefault();
     const textAreaValue = e.target[0].value;
 
     (async () => {
-      const response = await fetch(apiUrl + "/ult", {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-          "user-token": localStorage.getItem("user-token"),
-        },
-        body: JSON.stringify({
+      try {
+        const { error, code, data } = await apiCall("/ult", "POST", {
           message: textAreaValue,
-        }),
-      });
-      const jsonResponse = await response.json();
+        });
 
-      const { error, code, data } = jsonResponse;
+        if (error) {
+          console.error(code, data);
+          return;
+        }
 
-      if (error) {
-        console.error("CODE:", code);
-        console.error(data);
+        console.log("Ult created correctly");
+      } catch (err) {
+        console.error(err);
       }
-      console.log("CODE:", code);
-      console.log("DATA:", data);
     })();
-  });
+  };
+
+  return (
+    <form className={styles["create-ult"]} onSubmit={handleSubmit}>
+      <textarea
+        name="ult-text"
+        placeholder="Write your feelings..."
+        autoCapitalize="sentences"
+        minLength={1}
+        maxLength={100}
+        className={styles["ult-text"]}
+      ></textarea>
+      {/* <input
+        className={styles["button-create"]}
+        type="submit"
+        name="Post ult"
+      /> */}
+      <button className={styles["button-create"]} type="submit">
+        Post ult
+      </button>
+    </form>
+  );
+}
+
+{
+  /* <script define:vars={{ apiUrl }}>
+
 </script> */
 }
