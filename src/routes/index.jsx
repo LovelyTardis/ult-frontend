89df665e37ae036fb780406a465d7f43678651ac
Albert <albert.tardis05@gmail.com>
2023-02-03 12:51:47 +0100

    Add ult route + new errorTexts

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -1,16 +1,28 @@
 import ErrorDisplay from "../components/ErrorDisplay";
 import Home from "../views/Home";
 import Profile from "../views/Profile";
+import Ult from "../views/Ult";
+
+const errorTexts = {
+  home: "Home cannot be loaded",
+  profile: "User not found or it was deleted",
+  ult: "Ult not found or it was deleted",
+};
 
 export const ROUTES = {
   home: {
     path: "/",
     element: <Home />,
-    errorElement: <ErrorDisplay message={"Home cannot be loaded"} code={500} />,
+    errorElement: <ErrorDisplay message={errorTexts.home} code={500} />,
   },
   profile: {
     path: "/profile/:username",
     element: <Profile />,
-    errorElement: <ErrorDisplay message={"User not found"} code={404} />,
+    errorElement: <ErrorDisplay message={errorTexts.profile} code={404} />,
+  },
+  ult: {
+    path: "/ult/:ultId",
+    element: <Ult />,
+    errorElement: <ErrorDisplay message={errorTexts.ult} code={404} />,
   },
 };
