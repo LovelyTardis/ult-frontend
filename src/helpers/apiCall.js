d64ce3672d3211bb65eb671d46d4885799c295d4
Albert <albert.tardis05@gmail.com>
2023-02-03 12:44:16 +0100

    Delete trycatch and Add signal

--- src/helpers/apiCall.js
+++ src/helpers/apiCall.js
@@ -1,31 +1,33 @@
-export const apiCall = async (path, method = "GET", body = {}) => {
+export const apiCall = async (
+  path,
+  method = "GET",
+  body = {},
+  signal = null
+) => {
   let call = null;
   const url = import.meta.env.VITE_DEVAPI;
 
-  try {
-    switch (method) {
-      case "POST":
-        call = await fetch(`${url}${path}`, {
-          method,
-          body: JSON.stringify(body),
-          headers: {
-            "Content-Type": "application/json",
-            "user-token": localStorage.getItem("user-token") || "",
-          },
-        });
-        break;
-      default:
-        call = await fetch(`${url}${path}`);
-        break;
-    }
-
-    const jsonCall = await call.json();
-    const { ...res } = jsonCall;
-
-    return {
-      ...res,
-    };
-  } catch (error) {
-    console.error(error);
+  switch (method) {
+    case "POST":
+      call = await fetch(`${url}${path}`, {
+        method,
+        body: JSON.stringify(body),
+        headers: {
+          "Content-Type": "application/json",
+          "user-token": localStorage.getItem("user-token") || "",
+        },
+        signal,
+      });
+      break;
+    default:
+      call = await fetch(`${url}${path}`, { signal });
+      break;
   }
+
+  const jsonCall = await call.json();
+  const { ...res } = jsonCall;
+
+  return {
+    ...res,
+  };
 };
--- src/helpers/tryConnection.js
+++ src/helpers/tryConnection.js
@@ -1,12 +1,13 @@
-export const tryConnection = async () => {
-  try {
-    await fetch(`${import.meta.env.VITE_DEVAPI}`);
-    return true;
-  } catch (_) {
-    return {
-      code: 500,
-      error: true,
-      data: "No connection with the server api.",
-    };
-  }
+export const tryConnection = async (signal = null) => {
+  const connected = await fetch(`${import.meta.env.VITE_DEVAPI}`, {
+    signal,
+  });
+
+  if (connected.ok) return true;
+
+  return {
+    code: 500,
+    error: true,
+    data: "No connection with the server api.",
+  };
 };
