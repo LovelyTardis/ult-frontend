dc2c90833c5ebd341eacebdf8c38856b23d9dda4
Albert <albert.tardis05@gmail.com>
2023-02-03 12:43:38 +0100

    Implement first iteration for the Context

--- src/index.jsx
+++ src/index.jsx
@@ -1,11 +1,19 @@
-import React from "react";
+import React, { createContext } from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import Layout from "./layouts/Layout";
 
+let initialContextValues = {
+  ults: {},
+  userId: "",
+};
+
+export const UltContext = createContext(initialContextValues);
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <Layout />
+    <UltContext.Provider value={UltContext}>
+      <Layout />
+    </UltContext.Provider>
   </React.StrictMode>
 );
