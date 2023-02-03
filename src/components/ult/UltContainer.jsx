7f535e4ece38162a1c27e20b9160e1e142380bce
Albert <albert.tardis05@gmail.com>
2023-02-03 12:48:03 +0100

    Simplify ult container

--- src/components/ult/UltContainer.jsx
+++ src/components/ult/UltContainer.jsx
@@ -1,47 +1,11 @@
-import { useEffect, useState } from "react";
+import { UltCard } from "./index";
 
-import type { UltType } from "../../types";
-
-import { apiCall, tryConnection } from "../../helpers";
-import UltCard from "./UltCard";
-import ErrorDisplay from "../ErrorDisplay";
-// import UltCard from "./UltCard.astro";
-
-function UltContainer() {
-  let error = false,
-    code,
-    data;
-  const [ults, setUlts] = useState<UltType[]>([]);
-
-  const fetchData = async () => {
-    try {
-      const connected = await tryConnection();
-      ({ error, code, data } =
-        connected !== true ? connected : await apiCall("/ult"));
-
-      if (error) throw new Error(code, data);
-
-      data.ults.forEach((ult: UltType) => {
-        setUlts((prev) => [...prev, ult]);
-      });
-    } catch (err) {
-      console.error(err);
-    }
-  };
-
-  useEffect(() => {
-    fetchData();
-  }, []);
-
+function UltContainer({ ultsToShow }) {
   return (
     <div className="ult-container">
-      {Array.isArray(ults) ? (
-        ults.map((ult: UltType) => {
-          return <UltCard key={ult._id} ult={ult} />;
-        })
-      ) : (
-        <ErrorDisplay message={data} code={code} />
-      )}
+      {Object.values(ultsToShow).map((ult) => {
+        return <UltCard key={ult._id} ult={ult} />;
+      })}
     </div>
   );
 }
