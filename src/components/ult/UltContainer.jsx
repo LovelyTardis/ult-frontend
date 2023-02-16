153c9b3fe8348284660927a5ea84f5a941d687c8
Albert <albert.tardis05@gmail.com>
2023-02-16 13:57:27 +0100

    Implement sort helper

--- src/components/ult/UltContainer.jsx
+++ src/components/ult/UltContainer.jsx
@@ -1,9 +1,13 @@
+import { ultsSort } from "../../helpers";
 import { UltCard } from "./index";
 
 function UltContainer({ ultsToShow }) {
+  const ults = Object.values(ultsToShow);
+  ultsSort(ults);
+
   return (
     <div className="ult-container">
-      {Object.values(ultsToShow).map((ult) => {
+      {ults.map((ult) => {
         return <UltCard key={ult._id} ult={ult} />;
       })}
     </div>
