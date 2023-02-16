2cd184019c7acac562080e84857e8dc3ec4316c6
Albert <albert.tardis05@gmail.com>
2023-02-16 13:58:06 +0100

    Create basic SideBar component

---
+++ src/components/sidebar/SideBar.jsx
@@ -0,0 +1,13 @@
+import "./SideBar.css";
+
+export default function SideBar() {
+  return (
+    <div>
+      {/* TODO: PROFILE IMAGE FOR THE LOGGED USER */}
+      <a href="/">Home</a>
+      {/* TODO: CHANGE THE PROFILE FOR THE LOGGED USER */}
+      <a href="/profile/LovelyTardis">Profile</a>
+      <a href="/">Options or idk</a>
+    </div>
+  );
+}
