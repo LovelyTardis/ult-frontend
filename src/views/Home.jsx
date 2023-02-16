f4a1c47a957ff8684f646912ce0b1947cc1087b2
Albert <albert.tardis05@gmail.com>
2023-02-16 14:02:52 +0100

    Implement load ults

--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -1,10 +1,46 @@
-import UltCreate from "../components/ult/UltCreate";
+import { useEffect, useState } from "react";
+import { UltCreate, UltContainer } from "../components/ult";
+import { apiCall, tryConnection } from "../helpers";
 
 export default function Home() {
+  const [ults, setUlts] = useState({});
+
+  const fetchData = async (controller = new AbortController()) => {
+    try {
+      const connected = await tryConnection(controller.signal);
+      const { error, code, data } =
+        connected !== true
+          ? connected
+          : await apiCall("/ult", "GET", {}, controller.signal);
+
+      if (error) throw new Error(code, data);
+
+      setUlts(data.ults);
+    } catch (err) {
+      if (err.name !== "AbortError") console.error({ data, code });
+    }
+  };
+
+  useEffect(() => {
+    document.title = "Ult - Home";
+    let controller = new AbortController();
+    fetchData(controller);
+    return () => {
+      controller.abort();
+    };
+  }, []);
+
+  const refreshUlts = () => {
+    fetchData();
+  };
+
   return (
     <>
-      <h1>HOME PAGE</h1>
-      <UltCreate />
+      <UltCreate refreshUlts={refreshUlts} />
+      <button onClick={refreshUlts}>
+        <span className="material-symbols-outlined">autorenew</span>
+      </button>
+      <UltContainer ultsToShow={ults} />
     </>
   );
 }
