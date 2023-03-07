4b711fb5841ae869f4e56eef5ba1abebc44937ce
Albert <albert.tardis05@gmail.com>
2023-03-07 18:56:07 +0100

    Modify SideBar style

--- src/components/sidebar/SideBar.jsx
+++ src/components/sidebar/SideBar.jsx
@@ -1,13 +1,31 @@
+import { ROUTES } from "../../routes";
+import { CardHeader } from "../ult/card";
 import "./SideBar.css";
+import SideBarButton from "./SideBarButton";
 
 export default function SideBar() {
+  // FIXME: userData SHOULD BE GOT FROM REDUX
+  const userData = {
+    profilePicture:
+      "https://cdn.discordapp.com/avatars/327875616999342101/02a9744df4a438af25249b98ffac4986.webp?size=128",
+    username: "LovelyTardis",
+    name: "Albert",
+  };
+
   return (
-    <div>
-      {/* TODO: PROFILE IMAGE FOR THE LOGGED USER */}
-      <a href="/">Home</a>
-      {/* TODO: CHANGE THE PROFILE FOR THE LOGGED USER */}
-      <a href="/profile/LovelyTardis">Profile</a>
-      <a href="/">Options or idk</a>
+    <div className="sidebar-container">
+      <CardHeader user={userData} />
+      <hr />
+      {Object.values(ROUTES).map(({ href, text, icon }) => {
+        if (!href || !text || !icon) return;
+
+        const props = {
+          href,
+          text,
+          icon,
+        };
+        return <SideBarButton key={href} props={props} />;
+      })}
     </div>
   );
 }
