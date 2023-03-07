f31e3f278106c1bb7cdef24109356d20ffe1956b
Albert <albert.tardis05@gmail.com>
2023-03-07 18:59:17 +0100

    Include all views in an index.js file

---
+++ src/views/index.js
@@ -0,0 +1,5 @@
+export { default as Home } from "./Home.jsx";
+export { default as Profile } from "./Profile.jsx";
+export { default as Ult } from "./Ult.jsx";
+export { default as Settings } from "./Settings.jsx";
+export { default as Logout } from "./Logout.jsx";
