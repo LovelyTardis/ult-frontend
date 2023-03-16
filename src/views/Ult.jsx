cf49b4a1f8cbe869b7b336d961d2c7d69d04a20e
Albert <albert.tardis05@gmail.com>
2023-03-16 13:48:17 +0100

    Implement show ult comments

--- src/views/Ult.jsx
+++ src/views/Ult.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
-import { UltCard } from "../components/ult";
+import { UltCard, UltContainer } from "../components/ult";
 import ViewTitle from "../components/ViewTitle";
 import { apiCall, tryConnection } from "../helpers";
 
@@ -35,6 +35,14 @@ export default function Ult() {
       <>
         <ViewTitle title="Ult viewer" />
         <UltCard ult={ult} />
+        {ult.comments.length > 0 ? (
+          <>
+            <h4>Comments</h4>
+            <UltContainer ultsToShow={ult.comments} />
+          </>
+        ) : (
+          <h4>No comments for this ULT</h4>
+        )}
       </>
     )
   );
