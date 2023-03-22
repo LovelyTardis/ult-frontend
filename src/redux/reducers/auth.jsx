2833b7734936ca2a5c02225feb5068d5d4fd5775
Albert <albert.tardis05@gmail.com>
2023-03-22 13:19:10 +0100

    Create first reducer for auth

---
+++ src/redux/reducers/auth.jsx
@@ -0,0 +1,12 @@
+export const authReducer = (state = {}, action) => {
+  switch (action.type) {
+    case "LOGIN":
+      break;
+    case "LOGOUT":
+      break;
+    case "REGISTER":
+      break;
+    default:
+      break;
+  }
+};
