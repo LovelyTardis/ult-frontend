a9b60d07afd7f50bbff5a335719ca1b71cc1b62a
Albert <albert.tardis05@gmail.com>
2023-03-27 17:06:47 +0200

    Add Login export

--- src/views/index.js
+++ src/views/index.js
@@ -3,3 +3,4 @@ export { default as Profile } from "./Profile.jsx";
 export { default as Ult } from "./Ult.jsx";
 export { default as Settings } from "./Settings.jsx";
 export { default as Logout } from "./Logout.jsx";
+export { default as Login } from "./Login.jsx";
