e8de4ff0cc9314f60b5e5aa7481dab37a017ba74
Albert <albert.tardis05@gmail.com>
2023-03-27 16:41:04 +0200

    Implement change of the title in ViewTitle component

--- src/components/ViewTitle.jsx
+++ src/components/ViewTitle.jsx
@@ -1,3 +1,9 @@
+import { useEffect } from "react";
+
 export default function ViewTitle({ title }) {
+  useEffect(() => {
+    document.title = `ULT - ${title}`;
+  }, []);
+
   return <h2>{title}</h2>;
 }
