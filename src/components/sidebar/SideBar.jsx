db15814b0d88a72ffeadc0d50be5205c99b5fae1
Albert <albert.tardis05@gmail.com>
2023-03-28 12:36:53 +0200

    Implement redux in SideBar

--- src/components/sidebar/SideBar.jsx
+++ src/components/sidebar/SideBar.jsx
@@ -1,29 +1,50 @@
+import { useEffect, useState } from "react";
+import useAuth from "../../hooks/useAuth";
 import { ROUTES } from "../../routes";
 import { CardHeader } from "../ult/card";
 import "./SideBar.css";
 import SideBarButton from "./SideBarButton";
 
 export default function SideBar() {
-  // FIXME: userData SHOULD BE GOT FROM REDUX
-  const userData = {
-    profilePicture:
-      "https://cdn.discordapp.com/avatars/327875616999342101/02a9744df4a438af25249b98ffac4986.webp?size=128",
-    username: "LovelyTardis",
-    name: "Albert",
+  const { isAuth, user } = useAuth();
+
+  const initialValues = {
+    profilePicture: `${import.meta.env.VITE_API}${
+      import.meta.env.VITE_ANONYM_PATH
+    }`,
+    username: "anonymous",
+    name: "anonymous",
   };
+
+  const [userData, setUserData] = useState(isAuth ? user : initialValues);
+  useEffect(() => {
+    setUserData(isAuth ? user : initialValues);
+  }, [isAuth]);
 
   return (
     <div className="sidebar-container">
       <CardHeader user={userData} />
       <hr />
-      {Object.values(ROUTES).map(({ href, text, icon }) => {
+      {Object.values(ROUTES).map(({ href, text, icon, isPrivate }) => {
         if (!href || !text || !icon) return;
+        // TODO: CHECK IF USER IS AUTHENTICATED AND CREATE SIDEBAR BUTTON
+        // if (authReducer) {
+        //   // return;
+        // }
+
+        // // TODO: PRIVATE - PUBLIC ROUTES
+        // if (href === "/profile") {
+        //   href += `/${authReducer.username}`;
+        // }
 
         const props = {
           href,
           text,
           icon,
         };
+        // if (isPrivate) {
+        //   return <SideBarButton key={href} props={props} />;
+        // }
         return <SideBarButton key={href} props={props} />;
       })}
     </div>
