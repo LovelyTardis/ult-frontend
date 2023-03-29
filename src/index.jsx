767d6d5fe6b83eb0d180773394c883e722e14a27
Albert <albert.tardis05@gmail.com>
2023-03-29 18:21:19 +0200

    Remove strictmode

--- src/index.jsx
+++ src/index.jsx
@@ -9,9 +9,7 @@ import Layout from "./layouts/Layout";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
-    <Provider store={store}>
-      <Layout />
-    </Provider>
-  </React.StrictMode>
+  <Provider store={store}>
+    <Layout />
+  </Provider>
 );
