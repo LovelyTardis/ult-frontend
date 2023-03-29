55c722b0c8eeda345e0a3c5a913db6075994592b
Albert <albert.tardis05@gmail.com>
2023-03-29 14:04:56 +0200

    Create buttonProps file

---
+++ src/components/sidebar/buttonProps.js
@@ -0,0 +1,9 @@
+const buttonProps = {
+  home: { href: "/", text: "Home", icon: "home" },
+  profile: { href: "/profile", text: "My profile", icon: "person" },
+  settings: { href: "/settings", text: "Settings", icon: "settings" },
+  login: { href: "/login", text: "Login/Register", icon: "login" },
+  logout: { href: "/logout", text: "Logout", icon: "logout" },
+};
+
+export default buttonProps;
