039e45dcf130784d8ccbcb02d6ac2ba5fc0c7f98
Albert <albert.tardis05@gmail.com>
2023-03-29 18:20:49 +0200

    Add register route

--- src/layouts/Layout.jsx
+++ src/layouts/Layout.jsx
@@ -1,14 +1,17 @@
-import { useEffect, useState } from "react";
-import {
-  BrowserRouter as Router,
-  Navigate,
-  Route,
-  Routes,
-} from "react-router-dom";
+import { useEffect } from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import { RequireAuth } from "../components/auth";
 import SideBar from "../components/sidebar/SideBar";
 import useAuth from "../hooks/useAuth";
-import { Home, Login, Logout, Profile, Settings, Ult } from "../views";
+import {
+  Home,
+  Login,
+  Logout,
+  Profile,
+  Register,
+  Settings,
+  Ult,
+} from "../views";
 
 import "./Layout.css";
 
@@ -35,6 +38,7 @@ export default function Layout() {
               </Route>
               {/* PUBLIC ROUTES */}
               <Route path="/login" element={<Login />} />
+              <Route path="/register" element={<Register />} />
               <Route path="/profile/:username" element={<Profile />} />
               <Route path="/ult/:ultId" element={<Ult />} />
             </Routes>
