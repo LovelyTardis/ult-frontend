de813532eac899249d169f5dc782e9b8f492c7c4
Albert <albert.tardis05@gmail.com>
2023-03-31 01:36:32 +0200

    Implement fetching from useAuth hook

--- src/layouts/Layout.jsx
+++ src/layouts/Layout.jsx
@@ -1,4 +1,5 @@
 import { useEffect } from "react";
+import { useSelector } from "react-redux";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import { RequireAuth } from "../components/auth";
 import SideBar from "../components/sidebar/SideBar";
@@ -12,40 +13,39 @@ import {
   Settings,
   Ult,
 } from "../views";
+import NotFound from "../views/404";
 
 import "./Layout.css";
 
 export default function Layout() {
-  const { autoLogin, isAuth } = useAuth();
-
+  const { autoLogin, isAuth, fetching } = useAuth();
   useEffect(() => {
     autoLogin();
-  }, [isAuth]);
+  }, []);
 
-  return (
-    <>
-      <div className="container">
-        <Router>
-          <section id="router">
-            <Routes>
-              {/* PRIVATE ROUTES */}
-              <Route element={<RequireAuth auth={isAuth} />}>
-                <Route path="/" element={<Home />} />
-                <Route path="/settings" element={<Settings />} />
-                <Route path="/logout" element={<Logout />} />
-              </Route>
-              {/* PUBLIC ROUTES */}
-              <Route path="/login" element={<Login />} />
-              <Route path="/register" element={<Register />} />
-              <Route path="/profile/:username" element={<Profile />} />
-              <Route path="/ult/:ultId" element={<Ult />} />
-            </Routes>
-          </section>
-          <section id="sidebar">
-            <SideBar />
-          </section>
-        </Router>
-      </div>
-    </>
+  return fetching ? null : (
+    <div className="container">
+      <Router>
+        <section id="router">
+          <Routes>
+            {/* PRIVATE ROUTES */}
+            <Route element={<RequireAuth auth={isAuth} />}>
+              <Route path="/" element={<Home />} />
+              <Route path="/settings" element={<Settings />} />
+              <Route path="/logout" element={<Logout />} />
+            </Route>
+            {/* PUBLIC ROUTES */}
+            <Route path="/login" element={<Login />} />
+            <Route path="/register" element={<Register />} />
+            <Route path="/profile/:username" element={<Profile />} />
+            <Route path="/ult/:ultId" element={<Ult />} />
+            <Route path="*" element={<NotFound />} />
+          </Routes>
+        </section>
+        <section id="sidebar">
+          <SideBar />
+        </section>
+      </Router>
+    </div>
   );
 }
