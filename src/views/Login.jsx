88400ec4caa606530c757f414570c93cc580ff32
Albert <albert.tardis05@gmail.com>
2023-03-30 13:30:18 +0200

    Implement useNavigate hook

--- src/views/Login.jsx
+++ src/views/Login.jsx
@@ -1,8 +1,18 @@
+import { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
 import { LoginForm } from "../components/auth/LoginForm";
 import NavigateButton from "../components/NavigateButton";
 import ViewTitle from "../components/ViewTitle";
+import useAuth from "../hooks/useAuth";
 
 export default function Login() {
+  const { isAuth } = useAuth();
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    isAuth && navigate("/");
+  }, [isAuth]);
+
   return (
     <div>
       <ViewTitle title="Login" />
