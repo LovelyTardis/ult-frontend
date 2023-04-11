9c68e9d36f3f4dce9cc497118acc4ac19f75ca8c
Albert <albert.tardis05@gmail.com>
2023-04-11 12:13:30 +0200

    Add https support and custom port in .env

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,12 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig, loadEnv } from "vite";
+import react from "@vitejs/plugin-react-swc";
+import mkcert from "vite-plugin-mkcert";
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-})
+export default ({ mode }) => {
+  process.env = Object.assign(process.env, loadEnv(mode, process.cwd(), ""));
+  return defineConfig({
+    server: { port: process.env.VITE_PORT, https: true },
+    plugins: [react(), mkcert()],
+  });
+};
