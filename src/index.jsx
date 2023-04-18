110774f14a1e21ed748c12eb6f4a288b8153f842
Albert <albert.tardis05@gmail.com>
2023-04-18 11:48:17 +0200

    Implement Layout to App change

--- src/index.jsx
+++ src/index.jsx
@@ -5,11 +5,11 @@ import "./index.css";
 import { Provider } from "react-redux";
 import { store } from "./redux/store";
 
-import Layout from "./layouts/Layout";
+import App from "./App";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <Provider store={store}>
-    <Layout />
+    <App />
   </Provider>
 );
