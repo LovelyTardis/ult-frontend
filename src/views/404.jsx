942c08910d08b80d7566e57e32ab0490185c460c
Albert <albert.tardis05@gmail.com>
2023-04-18 11:48:51 +0200

    Redesign NotFound component with current not found param

--- src/views/404.jsx
+++ src/views/404.jsx
@@ -1,5 +1,21 @@
-function NotFound() {
-  return <h1>404 page</h1>;
-}
+import { useNavigate, useParams } from "react-router-dom";
 
-export default NotFound;
+export default function NotFound() {
+  const navigate = useNavigate();
+  const params = useParams();
+  const [param, value] = Object.entries(params)[0];
+
+  const handleClick = () => navigate("/");
+
+  return (
+    <div>
+      <h1 key={param + value}>
+        {param !== "*"
+          ? param.charAt(0).toUpperCase() + param.slice(1)
+          : "Page"}{" "}
+        "{value}" not found
+      </h1>
+      <button onClick={handleClick}>Return home</button>
+    </div>
+  );
+}
